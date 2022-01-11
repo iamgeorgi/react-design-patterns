bf4fe62aed5d998846e6f7372df7004cc152f52b
Georgi Petrov <georgipeetrov@gmail.com>
2022-01-11 12:06:40 +0200

    list items components and modals added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import { SplitScreen } from "./SplitScreen";
+import { Modal } from "./Modal";
+import { NumberedList } from "./Numberedlist";
+import { LargeProuctlistItem } from "./people/LargePersonListItem";
+import { SmallProuctlistItem } from "./people/SmallPersonListItem";
+import { LargePersonListItem } from "./products/LargeProductListItem";
+import { SmallPersonListItem } from "./products/SmallProuctListItem";
+import { RegularList } from "./RegularList";
 
-const LeftHandcomponent = ({ message }) => {
-  return <h1 style={{ backgroundColor: 'green' }}>{message}</h1>
-}
+const people = [{
+	name: 'John Doe',
+	age: 54,
+	hairColor: 'brown',
+	hobbies: ['swimming', 'bicycling', 'video games'],
+}, {
+	name: 'Brenda Smith',
+	age: 33,
+	hairColor: 'black',
+	hobbies: ['golf', 'mathematics'],
+}, {
+	name: 'Jane Garcia',
+	age: 27,
+	hairColor: 'blonde',
+	hobbies: ['biology', 'medicine', 'gymnastics'],
+}];
 
-const RightHandcomponent = ({ message }) => {
-  return <p style={{ backgroundColor: 'red' }}>{message}</p>
-}
+const products = [{
+	name: 'Flat-Screen TV',
+	price: '$300',
+	description: 'Huge LCD screen, a great deal',
+	rating: 4.5,
+}, {
+	name: 'Basketball',
+	price: '$10',
+	description: 'Just like the pros use',
+	rating: 3.8,
+}, {
+	name: 'Running Shoes',
+	price: '$120',
+	description: 'State-of-the-art technology for optimum running',
+	rating: 4.2,
+}];
 
 function App() {
-  return (
-      <SplitScreen 
-      leftWeight={1}
-      rightWeight={3} 
-      >
-        <LeftHandcomponent message="John" />
-        <RightHandcomponent message="Shaun" />
-      </SplitScreen>
-  );
+	return (
+		<>
+			<RegularList
+				items={people}
+				resourceName="person" // Small and LargePersonListItem gets it as a prop
+				itemComponent={SmallPersonListItem}
+			/>
+
+			<RegularList
+				items={people}
+				resourceName="person" // Small and LargePersonListItem gets it as a prop
+				itemComponent={LargePersonListItem}
+			/>
+
+			<NumberedList
+				items={products}
+				resourceName="product" // Small and LargePersonListItem gets it as a prop
+				itemComponent={SmallProuctlistItem}
+			/>
+			<NumberedList
+				items={products}
+				resourceName="product" // Small and LargePersonListItem gets it as a prop
+				itemComponent={LargeProuctlistItem}
+			/>
+			<Modal>
+				<LargeProuctlistItem product={products[0]}/>
+			</Modal>
+		</>
+	);
 }
 
 export default App;
