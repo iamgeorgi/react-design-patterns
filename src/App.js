8426b0bf54380213dd761808eb80e98a77816776
Georgi Petrov <georgipeetrov@gmail.com>
2022-01-14 18:46:27 +0200

    some components improved

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 
 const StepOne = ({ goToNext }) => (
 	<>
-	<h1>Step 1</h1>
-	<button onClick={goToNext}>Next</button>
+		<h1>Step 1</h1>
+		<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
 	</>
 );
 const StepTwo = ({ goToNext }) => (
 	<>
-	<h1>Step 2</h1>
-	<button onClick={goToNext}>Next</button>
+		<h1>Step 2</h1>
+		<button onClick={() => goToNext({ age: 100 })}>Next</button>
 	</>
 );
 const StepThree = ({ goToNext }) => (
 	<>
-	<h1>Step 3</h1>
-	<button onClick={goToNext}>Next</button>
+		<h1>Step 3</h1>
+		<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
 	</>
 );
 
@@ -39,7 +39,10 @@ function App() {
 			<button onClick={() => setShouldShowModal(!shouldShowModal)}>
 				{shouldShowModal ? "Hide Modal" : "Show Modal"}
 			</button>
-			<UncontrolledOnboardingFlow>
+			<UncontrolledOnboardingFlow onFinish={data => {
+				console.log(data);
+				alert('Onboarding complete!');
+			}}>
 				<StepOne />
 				<StepTwo />
 				<StepThree />
